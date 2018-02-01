0676f265bee0d7831f5b8c40b60ee9ebc83d79c0
Adam Langevin <langer1911@gmail.com>
2018-02-01 17:31:46 -0500

    PA3 done"

--- CondCode/PA3.m
+++ CondCode/PA3.m
@@ -1,78 +1,123 @@
-function  PA3(N, M, r, l, t, b)
-%20 x 20 mesh
-if ~isempty(N) && ~isempty(M)
-    n = N;
-    m = M;
-else
-    n = 20;     %x direction
-    m = 20;     %y direction
+function  PA3(N, M, r, l, t, b, insulTB, insulRL)
 
+n = N;
+m = M;
+
+right = r;
+left = l;
+top = t;
+bot = b;
+
+if insulTB == 1
+    
 end
 
-if t>0 & b>0 & r>0 & l>0
-    right = r;
-    left = l;
-    top = t;
-    bot = b;
-else
-    right = 1;
-    left = 0;
-    top = 0;
-    bot = 0;
+if insulRL ==1 
+    
 end
-T1 = top;
-T2 = bot;
 
 global V;
 
 for i = 1:m
     for j = 1:n
         if i-1 <= 0                             %top wall
-            if j-1 <= 0
-                V(i,j) = (V(i+1,j) + V(i,j) + V(i,j+1) + left)/4;
-                
-            elseif j+1 > n
-                V(i,j) = (V(i+1,j) + left + V(i,j) + V(i,j-1))/4;
-                
+            if insulTB == 0
+                if j-1 <= 0
+                    V(i,j) = (V(i+1,j) + top + V(i,j+1) + left)/4;
+
+                elseif j+1 > n
+                    V(i,j) = (V(i+1,j) + right + top + V(i,j-1))/4;
+
+                else
+                    V(i,j) = (V(i+1,j) + top + V(i,j+1) + V(i,j-1))/4;
+
+                end
             else
-                V(i,j) = (V(i+1,j) + left + V(i,j+1) + V(i,j-1))/4;
-                
+               if j-1 <= 0
+                    V(i,j) = (V(i+1,j) + V(i,j) + V(i,j+1) + left)/4;
+
+                elseif j+1 > n
+                    V(i,j) = (V(i+1,j) + right + V(i,j) + V(i,j-1))/4;
+
+                else
+                    V(i,j) = (V(i+1,j) + V(i,j) + V(i,j+1) + V(i,j-1))/4;
+
+                end 
             end
         end
         if i+1 > m                              %bottom wall
-            if j-1 <= 0
-                V(i,j) = (V(i-1,j) + V(i,j) + V(i,j+1) + left)/4;
-                
-            elseif j+1 > n 
-                V(i,j) = (V(i,j) + V(i-1,j) + right + V(i,j-1))/4;
-                
+            if insulTB == 0
+                if j-1 <= 0
+                    V(i,j) = (V(i-1,j) + bot + V(i,j+1) + left)/4;
+
+                elseif j+1 > n 
+                    V(i,j) = (bot + V(i-1,j) + right + V(i,j-1))/4;
+
+                else
+                    V(i,j) = (bot + V(i-1,j) + V(i,j+1) + V(i,j-1))/4;
+
+                end
             else
-                V(i,j) = (V(i,j) + V(i-1,j) + V(i,j+1) + V(i,j-1))/4;
-                
+                if j-1 <= 0
+                    V(i,j) = (V(i-1,j) + V(i,j) + V(i,j+1) + left)/4;
+
+                elseif j+1 > n 
+                    V(i,j) = (V(i,j) + V(i-1,j) + right + V(i,j-1))/4;
+
+                else
+                    V(i,j) = (V(i,j) + V(i-1,j) + V(i,j+1) + V(i,j-1))/4;
+
+                end
             end
         end
         if j-1 <= 0                            %left nodes
-            if i-1<=0
-                V(i,j) = (V(i,j) + V(i+1,j) + V(i,j+1) + left)/4;
-                
-            elseif i+1>m
-                V(i,j) = (V(i-1,j) + V(i,j) + V(i,j+1) + left)/4;
-                
+            if insulTB ==0
+                if i-1<=0
+                    %%V(i,j) = (V(i,j) + V(i+1,j) + V(i,j+1) + left)/4;
+
+                elseif i+1>m
+                    %V(i,j) = (V(i-1,j) + V(i,j) + V(i,j+1) + left)/4;
+
+                else
+                    V(i,j) = (V(i-1,j) + V(i+1,j) + V(i,j+1) + left)/4;
+
+                end 
             else
-                V(i,j) = (V(i-1,j) + V(i+1,j) + V(i,j+1) + left)/4;
-                
-            end          
+                if i-1<=0
+                    %V(i,j) = (top + V(i+1,j) + V(i,j+1) + left)/4;
+
+                elseif i+1>m
+                    %V(i,j) = (V(i-1,j) + bot + V(i,j+1) + left)/4;
+
+                else
+                    V(i,j) = (V(i-1,j) + V(i+1,j) + V(i,j+1) + left)/4;
+
+                end  
+            end
         end
         if j+1>n                               %right nodes
-            if i-1<=0
-                V(i,j) = (V(i,j) + V(i+1,j) + right + V(i,j-1))/4;
-                
-            elseif i+1>m
-                V(i,j) = (V(i-1,j) + V(i,j) + right + V(i,j-1))/4;
-                
+            if insulTB ==0
+                 if i-1<=0
+                    %V(i,j) = (V(i,j) + V(i+1,j) + right + V(i,j-1))/4;
+
+                elseif i+1>m
+                    %V(i,j) = (V(i-1,j) + V(i,j) + right + V(i,j-1))/4;
+
+                else
+                    V(i,j) = (V(i-1,j) + V(i+1,j) + right + V(i,j-1))/4;
+
+                end
             else
-                V(i,j) = (V(i-1,j) + V(i+1,j) + right + V(i,j-1))/4;
-                
+                if i-1<=0
+                    %V(i,j) = (top + V(i+1,j) + right + V(i,j-1))/4;
+
+                elseif i+1>m
+                    %V(i,j) = (V(i-1,j) + bot + right + V(i,j-1))/4;
+
+                else
+                    V(i,j) = (V(i-1,j) + V(i+1,j) + right + V(i,j-1))/4;
+
+                end
             end
             
         end                      
@@ -80,8 +125,6 @@ for i = 1:m
             V(i,j) = (V(i+1,j) + V(i-1,j) + V(i,j+1) + V(i,j-1))/4;
         end
                 
-        T1 = V(i,j);      %for a bottom node
-        T2 = V(i,j);      %for a top node
     end
 end
 end
--- CondCode/PA3GetVolt.m
+++ CondCode/PA3GetVolt.m
@@ -3,16 +3,33 @@ clearvars -GLOBAL
 close all
 global V;
 
-N = 10;
-M = 10;
+N = 100;
+M = 100;
+R = 1;
+L = 0;
+T = 0;
+B = 0;
 
-V = zeros(N,M);
-numSims = 100;
+V = rand(N,M);
+numSims = 1000;
 
 figure(1);
-
+Ex = M;
+Ey = N;
 
 for i =1:numSims
-    PA3(N,M,[],[],[],[]);  
+    PA3(N,M,R,L,T,B,0,0); 
+    figure(1);
+    surf(1:M,1:N,V);
+    fprintf("percent complete: %g \n", i/numSims *100);
+    [Ex, Ey] = gradient(V);
+    figure(2);
+    surf(1:M,1:N,Ex);
+    figure(3);
+    surf(1:M,1:N,Ey);
+    figure(4);
+    quiver(Ex,Ey);
+    pause(0.00000001);
+
 end
 
